afb47f6b27494c54bbc39f7da0facdf5e98114ec
zakhaev26 <b422056@iiit-bh.ac.in>
2023-12-21 05:07:41 +0530

    adds benchmarks

--- controller/insertAdminData.go
+++ controller/insertAdminData.go
@@ -6,18 +6,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"sync"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
 	db "github.com/zakhaev26/microservices-go/database"
 	model "github.com/zakhaev26/microservices-go/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func HandleDataPost(w http.ResponseWriter, r *http.Request) {
 	var wg sync.WaitGroup
 	fmt.Println("AYA")
-
 	var incomingScore model.AdminData
 
 	wg.Add(1)
@@ -40,39 +39,61 @@ func HandleDataPost(w http.ResponseWriter, r *http.Request) {
 		}
 	}()
 
-	delivery_chan := make(chan kafka.Event, 10000)
-	topic := "Topic"
+	// delivery_chan := make(chan kafka.Event, 10000)
+	// topic := "Topic"
+
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+
+	// 	p, err := kafka.NewProducer(&kafka.ConfigMap{
+	// 		"bootstrap.servers": "127.0.0.1:9092",
+	// 		"acks":              "all",
+	// 	})
+
+	// 	if err != nil {
+	// 		fmt.Printf("Failed to produce message: %v\n", err)
+	// 	}
+	// 	jsonPayload, err := json.Marshal(incomingScore)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+
+	// 	// time.Sleep(time.Second * 6)
+	// 	err = p.Produce(&kafka.Message{
+	// 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+	// 		Value:          []byte(string(jsonPayload))},
+	// 		delivery_chan,
+	// 	)
+
+	// 	if err != nil {
+	// 		fmt.Printf("Failed to produce message: %v\n", err)
+	// 		os.Exit(1)
+	// 	}
+	// 	e := <-delivery_chan
+	// 	fmt.Println("Delivered", e.String())
+	// }()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
-		p, err := kafka.NewProducer(&kafka.ConfigMap{
-			"bootstrap.servers": "127.0.0.1:9092",
-			"acks":              "all",
-		})
+		collection := db.Collection
 
+		changeStreamOptions := options.ChangeStream().
+			SetFullDocument(options.UpdateLookup)
+		changeStream, err := collection.Watch(context.Background(), mongo.Pipeline{}, changeStreamOptions)
 		if err != nil {
-			fmt.Printf("Failed to produce message: %v\n", err)
-		}
-		jsonPayload, err := json.Marshal(incomingScore)
-		if err != nil {
-			fmt.Println(err)
+			log.Fatal(err)
 		}
 
-		// time.Sleep(time.Second * 6)
-		err = p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          []byte(string(jsonPayload))},
-			delivery_chan,
-		)
-
-		if err != nil {
-			fmt.Printf("Failed to produce message: %v\n", err)
-			os.Exit(1)
+		for changeStream.Next(context.Background()) {
+			changeEvent := changeStream.Current
+			fmt.Printf("Change Event: %+v\n", changeEvent)
+			json.NewEncoder(w).Encode(changeEvent)
+			return
 		}
-		e := <-delivery_chan
-		fmt.Println("Delivered", e.String())
+
 	}()
 
 	wg.Wait()
